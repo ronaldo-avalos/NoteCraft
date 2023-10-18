fa6cd83f091e276043f077d55d4b67f9fe1f8000
ronaldoavalos <ronaldo@vecinio.mx>
2023-10-18 11:59:36 -0600

    added HIlt

--- app/src/main/java/mx/copeertec/notecraft/MainActivity.kt
+++ app/src/main/java/mx/copeertec/notecraft/MainActivity.kt
@@ -13,8 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import mx.copeertec.notecraft.ui.theme.NoteCraftTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/mx/copeertec/notecraft/app/MayNoteCraft.kt
@@ -0,0 +1,8 @@
+package mx.copeertec.notecraft.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MayNoteCraft : Application() {
+}
