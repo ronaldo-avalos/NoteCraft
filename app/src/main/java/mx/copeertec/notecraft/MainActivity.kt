5fc6d610163f3552629532ecdb023b8f591b64f6
ronaldoavalos <ronaldo@vecinio.mx>
2023-10-18 11:09:56 -0600

    first commit

---
+++ app/src/main/java/mx/copeertec/notecraft/MainActivity.kt
@@ -0,0 +1,55 @@
+package mx.copeertec.notecraft
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.copeertec.notecraft.ui.theme.NoteCraftTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteCraftTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("NoteCraft")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+) {
+
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteCraftTheme {
+        Greeting("Android")
+    }
+}
